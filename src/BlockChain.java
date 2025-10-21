3f312918b2675aaccfb57962a2e510e22a2231fd
Thomas <olsoto@iastate.edu>
2025-10-21 18:02:53 -0500

    added function to BlockChain class to verify a chains validity

--- src/BlockChain.java
+++ src/BlockChain.java
@@ -4,6 +4,25 @@ import com.google.gson.GsonBuilder;
 public class BlockChain {
     public static ArrayList<Block> blockchain = new ArrayList<Block>();
 
+    public static boolean isChainValid(){
+        Block currentBlock;
+        Block previousBlock;
+
+        // loop through the blockchain to check hashes
+        for (int i = 1; i < blockchain.size(); i++){
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i - 1);
+            if (!currentBlock.hash.equals(currentBlock.calculateHash())){
+                System.out.println("Current Hashes not equal");
+                return false;
+            }
+            if (!previousBlock.hash.equals(currentBlock.previousHash)){
+                System.out.println("Previous Hashes not equal");
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
         blockchain.add(new Block("This is the first block", "0"));
         blockchain.add(new Block("This is the second block", blockchain.get(blockchain.size() - 1).hash));
